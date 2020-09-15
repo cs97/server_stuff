1a3faea532425306b6dca7138bc922117e620b7c
leftside97 <l3f7s1d3@protonmail.com>
2020-09-15 19:43:08 +0200

    Update salti.sh

--- salti.sh
+++ salti.sh
@@ -3,5 +3,5 @@ salt="299b61cc84d1ad3f68313ed267e2f425b39f841db4e2417eeff81623bdb018bab25d3a7ecd
 echo "key:"
 read input
 echo "new key:"
-echo $input$salt | sha256sum | base64 | head -c 32
+echo $input$salt | sha256sum | base64 | head -c 64
 echo ""
